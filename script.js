aff44f76a9e63ae343ec6ea9038abcb5e512f42b
KEVIN ANDRES CORZO GARCIA <136148374+Trullez-44@users.noreply.github.com>
2023-09-01 07:36:09 -0500

    card and video uploaded

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.getElementById('next').onclick = function (){
     let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').appendChild(lists[0]);  
-}
+    document.getElementById('slide').appendChild(lists[0]);}
 document.getElementById('prev').onclick = function (){
     let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').prepend(lists[lists.length - 1]);
-}
+    document.getElementById('slide').prepend(lists[lists.length - 1]);}
+    /* ******************************* */
+    const card =document.querySelector('.cardvideo');
+    const video =document.querySelector('.video');
+    card.addEventListener('mouseover', ()=>{video.play})
+    card.addEventListener('mouseleave', ()=>{video.pause})
